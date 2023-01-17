d8aa10889f70c83af1dd0acad4b323cb804c2074
AnaMarquez <ana_marquez@trimble.com>
2023-01-16 22:24:52 -0800

    Creating token service

---
+++ BlogLab/BlogLab.Servces/TokenService.cs
@@ -0,0 +1,35 @@
+﻿using BlogLab.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BlogLab.Services
+{
+    public class TokenService : ITokenService
+    {
+
+        private readonly SymmetricSecurityKey _key;
+        private readonly string _issuer;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            _issuer = config["Jwt:Issuer"];
+        }
+        public string CreateToken(ApplicationUserIdentity user)
+        {
+            var claimms = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.ApplicationUserId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username)
+            };
+
+            var cred = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
--- BlogLab/BlogLab.web/Program.cs
+++ BlogLab/BlogLab.web/Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+/*
+builder.Configuration.AddJsonFile("appsettings.json", true, true);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true);
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.Configure<CloudinaryOptions>(builder.Configuration.GetSection("CloudinaryOptions"));
+*/
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 

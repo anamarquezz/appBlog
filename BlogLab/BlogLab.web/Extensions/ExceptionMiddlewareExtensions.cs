d137db2625a4f331823cbe52f50a1f7e9601808d
AnaMarquez <ana_marquez@trimble.com>
2023-01-18 08:17:24 -0800

    update program.cs
    update program.cs

---
+++ BlogLab/BlogLab.web/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using BlogLab.Models.Exception;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace BlogLab.web.Extensions
+{
+    static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        //in production version of this app , you would log Exceptions in database
+                        //contextFeature.Error.Message;
+
+                        await context.Response.WriteAsync(new ApiException()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error"
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
--- BlogLab/BlogLab.web/Program.cs
+++ BlogLab/BlogLab.web/Program.cs
@@ -1,12 +1,71 @@
+using BlogLab.Models.Settings;
+using BlogLab.Repository;
+using BlogLab.Services;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using BlogLab.Models.Account;
+using BlogLab.Identity;
+using BlogLab.web.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
 /*
 builder.Configuration.AddJsonFile("appsettings.json", true, true);
 builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true);
 builder.Configuration.AddEnvironmentVariables();
+*/
 
 builder.Services.Configure<CloudinaryOptions>(builder.Configuration.GetSection("CloudinaryOptions"));
-*/
+
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+
+builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+builder.Services.AddScoped<IBlogCommentRepository, BlogCommentRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+builder.Services.AddIdentityCore<ApplicationUserIdentity>(opt =>
+    {
+        opt.Password.RequireNonAlphanumeric = false;
+
+    })
+    .AddUserStore<UserStore>()
+    .AddDefaultTokenProviders()
+    .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(
+        options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"])),
+                ClockSkew = TimeSpan.Zero
+            };
+        }
+    );
 
 
 // Add services to the container.
@@ -17,9 +76,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
@@ -27,7 +92,25 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.ConfigureExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+}
+else
+{
+    app.UseCors(options => options.WithOrigins("http://website.com"));
+}
+
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
 
 app.MapRazorPages();
 

aab4666d15e638068f7014d5caa49b2ea1dcb63d
AnaMarquez <ana_marquez@trimble.com>
2023-01-25 11:27:35 -0800

    building all the services

--- BlogLab/BlogLab-UI/src/app/services/blog.service.ts
+++ BlogLab/BlogLab-UI/src/app/services/blog.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BlogComment } from '../models/blog-comment/blog-comment.model';
+import { BlogCreate } from '../models/blog/blog-create.model';
+import { BlogPaging } from '../models/blog/blog-paging.model';
+import { Blog } from '../models/blog/blog.model';
+import { PageResult } from '../models/blog/paged-result.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(model: BlogCreate): Observable<Blog>{
+    return this.http.post<Blog>(`${environment.webapi}/Blog`, model);
+  }
+
+  getAll(blogPaging : BlogPaging) : Observable<PageResult<Blog>>{
+    return this.http.get<PageResult<Blog>>(
+      `${environment.webapi}/Blog?Page=${blogPaging.page}&PageSize=${blogPaging.pageSize})`); 
+  }
+  get(blogId: number): Observable<Blog> {
+    return this.http.get<Blog>(`${environment.webapi}/Blog/${blogId})`); 
+  }
+  getByApplicationUserId(applicationUserId: number) : Observable<Blog[]>{
+    return this.http.get<Blog[]>(`${environment.webapi}/Blog/user/${applicationUserId})`); 
+  }
+  getMostFamous(){
+    return this.http.get<Blog[]>(`${environment.webapi}/Blog/famous`); 
+  }
+  delete(blogId: number){
+    return this.http.get<number>(`${environment.webapi}/Blog/${blogId}`);
+  }
 }
--- BlogLab/BlogLab-UI/src/app/services/photo.service.ts
+++ BlogLab/BlogLab-UI/src/app/services/photo.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Photo } from '../models/photo/photo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(model: FormData): Observable<Photo>{
+    return this.http.post<Photo>(`${environment.webapi}/Photo`, model);
+    
+  }
+  getByApplicationuserId(): Observable<Photo[]>{
+    return this.http.get<Photo[]>(`${environment.webapi}/Photo`);
+
+  }
+
+  get(photoId: number): Observable<Photo>{
+    return this.http.get<Photo>(`${environment.webapi}/Photo/${photoId}`);
+  }
+  
+  delete(photoId: number): Observable<number>{
+    return this.http.delete<number>(`${environment.webapi}/Photo/${photoId}`);
+  }
 }
